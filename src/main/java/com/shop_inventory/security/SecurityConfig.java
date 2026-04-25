3f499312f6225e91c0d93b2e00cbd9034bb1a91e
Rahil Eqbal <rahil@example.com>
2026-04-25 15:16:30 +0530

    Change Cors new

--- src/main/java/com/shop_inventory/security/SecurityConfig.java
+++ src/main/java/com/shop_inventory/security/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOrigins(List.of("https://hardware-shop-frontend-git-main-eqbaltherebels-projects.vercel.app/"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
